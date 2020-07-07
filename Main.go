a421c3972a3f6c22e3887c1c8deda959978da116
Gabriel <gnunezyuve@pm.me>
2020-07-06 22:36:35 -0300

    Metemos regex y sacamos las líneas que se precisan, ahora a limpiar 🧹

--- Main.go
+++ Main.go
@@ -2,36 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"regexp"
 	"strings"
 )
 
 var (
 	subdURL     string = "http://www.subdivx.com/index.php?accion=5&masdesc=&subtitulos=1&realiza_b=1&q="
-	subdPayload string = "mr robot s03"
+	subdPayload string = "mr robot s03e01"
 	subdHeaders        = map[string]string{
 		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"}
 )
-
-func getPage(headers map[string]string, url string, payload string) []byte {
-
-	req, _ := http.NewRequest("GET", url+payload, nil)
-
-	req.Header.Add("User-Agent", headers["User-Agent"])
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-
-	body, _ := ioutil.ReadAll(res.Body)
-
-	return body
-}
 
 func main() {
 
@@ -39,9 +19,15 @@ func main() {
 
 	page := getPage(subdHeaders, subdURL, subdPayload)
 
-	titleStartIndex := strings.Index(string(page), "<Title>")
-	titleEndIndex := strings.Index(string(page), "</title>")
+	re := regexp.MustCompile("<div id=\"menu_detalle_buscador\">(.|\n)*?<div id=\"menu_detalle_buscador\">")
+	lines := re.FindAllString(string(page), -1)
+
+	titleStartIndex := strings.Index(string(page), "subidos")
+	titleEndIndex := strings.Index(string(page), "pabloaran")
 	pageTitle := []byte(string(page)[titleStartIndex:titleEndIndex])
+
 	fmt.Println(string(pageTitle))
+
+	fmt.Printf("%+q", lines)
 
 }
