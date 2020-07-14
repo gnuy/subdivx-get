ed2e39dd55a8ea47561516ddc6fa5bf89805b22d
Gabriel <gnunezyuve@pm.me>
2020-07-13 22:59:01 -0300

    Página chota que no usa UTF-8 me manda a salir a convertir su wea encodeada

--- Main.go
+++ Main.go
@@ -11,6 +11,7 @@ var (
 	listPayload string = "mr robot s03e01"
 	regex              = map[string]string{
 		"filterList": "<div id=\"menu_detalle_buscador\">(.|\n)*?</div></div>"}
+	subEjemplo string = "<div id=\"menu_detalle_buscador\"><div id=\"menu_titulo_buscador\"><a class=\"titulo_menu_izq\" href=\"http://www.subdivx.com/X6XNTIwNDUyX-mr-robot-s03e01.html\">Subtitulos de Mr. Robot S03E01</a></div><img src=\"img/calif2.gif\" class=\"detalle_calif\" name=\"detalle_calif\"></div><div id=\"buscador_detalle\">\n<div id=\"buscador_detalle_sub\">subidos por pabloaran y modificados para que coincidan con el capítulo de popcorn time  mr robot s03e01 eps3 0 power-saver-mode h 1080p amzn web-dl ddp5 1 h 264-ntb</div><div id=\"buscador_detalle_sub_datos\"><b>Downloads:</b> 1,046 <b>Cds:</b> 1 <b>Comentarios:</b> <a rel=\"nofollow\" href=\"popcoment.php?idsub=NTIwNDUy\" onclick=\"return hs.htmlExpand(this, { objectType: 'iframe' } )\">2</a> <b>Formato:</b> SubRip <b>Subido por:</b> <a class=\"link1\" href=\"http://www.subdivx.com/X9X1991429\">pincharata</a> <img src=\"/pais/1.gif\" width=\"16\" height=\"12\"> <b>el</b> 29/11/2017 </div></div>"
 )
 
 type subElement struct {
@@ -23,6 +24,18 @@ type subElement struct {
 	score     string
 }
 
+// func main() {
+// 	fmt.Printf("%s", subEjemplo)
+// }
+
+func toUtf8(iso8859_1_buf []byte) string {
+	buf := make([]rune, len(iso8859_1_buf))
+	for i, b := range iso8859_1_buf {
+		buf[i] = rune(b)
+	}
+	return string(buf)
+}
+
 func main() {
 
 	listPayload := strings.ReplaceAll(listPayload, " ", "%20")
@@ -31,12 +44,13 @@ func main() {
 	page := getPage(listURL, listPayload)
 
 	re := regexp.MustCompile(regex["filterList"])
-	lines := re.FindAllString(string(page), -1)
+	// lines := re.FindAllString(string(page), -1)
+	lines := re.FindAll(page, -1)
 
 	fmt.Println("\n\n", len(lines))
 
 	for i := 0; i < len(lines); i++ {
-		fmt.Printf("%s%+q%s\n\n", "⭐", lines[i], "⭐")
+		fmt.Printf("%s%s%s\n\n", "⭐", toUtf8(lines[i]), "⭐")
 	}
 
 }
--- Page.go
+++ Page.go
@@ -8,7 +8,8 @@ import (
 
 var (
 	subdHeaders = map[string]string{
-		"User-Agent": "🦊Mozilla🦊/5.0 (X11; 🐧Linux🐧 x86_64; rv:68.0) 🦎Gecko🦎/20100101 🔥Firefox🔥/68.0"}
+		"User-Agent":     "🦊Mozilla🦊/5.0 (X11; 🐧Linux🐧 x86_64; rv:68.0) 🦎Gecko🦎/20100101 🔥Firefox🔥/68.0",
+		"Accept-Charset": "utf-8"}
 )
 
 func getPage(url string, payload string) []byte {
@@ -16,6 +17,7 @@ func getPage(url string, payload string) []byte {
 	req, _ := http.NewRequest("GET", url+payload, nil)
 
 	req.Header.Add("User-Agent", subdHeaders["User-Agent"])
+	req.Header.Add("Accept-Charset", subdHeaders["Accept-Charset"])
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
