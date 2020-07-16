c251dc4ca1e4828742ea9b1272832fd96b92f8cc
Gabriel <gnunezyuve@pm.me>
2020-07-16 00:47:26 -0300

    Se implementa función `extract` para obtener un campo de la página dada una regex

--- Main.go
+++ Main.go
@@ -10,7 +10,8 @@ var (
 	listURL     string = "http://www.subdivx.com/index.php?accion=5&masdesc=&subtitulos=1&realiza_b=1&q="
 	listPayload string = "mr robot s03e01"
 	regex              = map[string]string{
-		"filterList": "<div id=\"menu_detalle_buscador\">(.|\n)*?</div></div>"}
+		"filterList": "<div id=\"menu_detalle_buscador\">(.|\n)*?</div></div>",
+		"getLink":    "<a class=\"titulo_menu_izq\" href=\"(.)*?\">"}
 	subEjemplo string = "<div id=\"menu_detalle_buscador\"><div id=\"menu_titulo_buscador\"><a class=\"titulo_menu_izq\" href=\"http://www.subdivx.com/X6XNTIwNDUyX-mr-robot-s03e01.html\">Subtitulos de Mr. Robot S03E01</a></div><img src=\"img/calif2.gif\" class=\"detalle_calif\" name=\"detalle_calif\"></div><div id=\"buscador_detalle\">\n<div id=\"buscador_detalle_sub\">subidos por pabloaran y modificados para que coincidan con el capítulo de popcorn time  mr robot s03e01 eps3 0 power-saver-mode h 1080p amzn web-dl ddp5 1 h 264-ntb</div><div id=\"buscador_detalle_sub_datos\"><b>Downloads:</b> 1,046 <b>Cds:</b> 1 <b>Comentarios:</b> <a rel=\"nofollow\" href=\"popcoment.php?idsub=NTIwNDUy\" onclick=\"return hs.htmlExpand(this, { objectType: 'iframe' } )\">2</a> <b>Formato:</b> SubRip <b>Subido por:</b> <a class=\"link1\" href=\"http://www.subdivx.com/X9X1991429\">pincharata</a> <img src=\"/pais/1.gif\" width=\"16\" height=\"12\"> <b>el</b> 29/11/2017 </div></div>"
 )
 
@@ -24,9 +25,18 @@ type subElement struct {
 	score     string
 }
 
-// func main() {
-// 	fmt.Printf("%s", subEjemplo)
-// }
+func extract(line string, field string) string {
+	re := regexp.MustCompile(regex[field])
+	strParts := strings.Split(regex[field], "(.)*?")
+	raw := re.FindString(line)
+	parsedValue := raw[len(strParts[0]) : len(raw)-len(strParts[1])]
+	return parsedValue
+
+}
+
+func main() {
+	fmt.Println(extract(subEjemplo, "getLink"))
+}
 
 func toUtf8(iso8859_1buf []byte) string {
 	buf := make([]rune, len(iso8859_1buf))
@@ -36,20 +46,20 @@ func toUtf8(iso8859_1buf []byte) string {
 	return string(buf)
 }
 
-func main() {
+// func main() {
 
-	listPayload := strings.ReplaceAll(listPayload, " ", "%20")
+// 	listPayload := strings.ReplaceAll(listPayload, " ", "%20")
 
-	fmt.Println(listURL + listPayload + "\n" + subdHeaders["User-Agent"])
-	page := getPage(listURL, listPayload)
+// 	fmt.Println(listURL + listPayload + "\n" + subdHeaders["User-Agent"])
+// 	page := getPage(listURL, listPayload)
 
-	re := regexp.MustCompile(regex["filterList"])
-	lines := re.FindAll(page, -1)
+// 	re := regexp.MustCompile(regex["filterList"])
+// 	lines := re.FindAll(page, -1)
 
-	fmt.Println("\n\n", len(lines))
+// 	fmt.Println("\n\n", len(lines))
 
-	for i := 0; i < len(lines); i++ {
-		fmt.Printf("%s%s%s\n\n", "⭐", toUtf8(lines[i]), "⭐")
-	}
+// 	for i := 0; i < len(lines); i++ {
+// 		fmt.Printf("%s%s%s\n\n", "⭐", toUtf8(lines[i]), "⭐")
+// 	}
 
-}
+// }
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.14
 
 require (
 	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/frankban/quicktest v1.10.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/mholt/archiver v3.1.1+incompatible
+	github.com/mholt/archiver v3.1.1+incompatible // indirect
 	github.com/mholt/archiver/v3 v3.3.0
 	github.com/nwaples/rardecode v1.1.0 // indirect
 	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
